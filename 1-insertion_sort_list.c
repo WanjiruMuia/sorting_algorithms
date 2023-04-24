f464881aa7d50743aeea3f808f27121d3868e6e1
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-24 23:37:48 +0300

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       in ascending order using the Insertion sort algorithm
- * @list: Pointer to pointer to the head of the doubly linked list
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ * @list: Dobule linked list to sort
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
+	listint_t *node;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+/**
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
 
-    current = (*list)->next;
-
-    while (current != NULL)
-    {
-        temp = current->prev;
-        while (temp != NULL && temp->n > current->n)
-        {
-            /* Swap the nodes */
-            if (temp->prev != NULL)
-                temp->prev->next = current;
-            else
-                *list = current;
-
-            current->prev = temp->prev;
-            temp->prev = current;
-            temp->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = temp;
-            current->next = temp;
-
-            /* Print the list after each swap */
-            print_list(*list);
-            temp = current->prev;
-        }
-        current = current->next;
-    }
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
