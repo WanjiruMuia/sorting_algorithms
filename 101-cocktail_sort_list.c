d6a952f0fcc420399afd9779f4d34aef43d5727c
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-25 00:15:08 +0300

    task 6

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,99 +1,67 @@
 #include "sort.h"
-
+#include <stdio.h>
 /**
- * cocktail_sort_list - sorts a doubly linked list of integers in ascending order
- *                      using the Cocktail shaker sort algorithm
- * @list: pointer to head of the doubly linked list
+ *swap_node - swap a node for his previous one
+ *@node: node
+ *@list: node list
+ *Return: return a pointer to a node which was enter it
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
+/**
+ *cocktail_sort_list - this is a cocktail sort implementation
+ *working on a double linked lists
+ *@list: list
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *start = NULL, *end = NULL, *curr = NULL;
-    int swapped = 0;
+	listint_t *node;
+	int swap_done = 1;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-
-    do {
-        swapped = 0;
-        for (curr = start = (*list); curr->next != end; curr = curr->next)
-        {
-            if (curr->n > curr->next->n)
-            {
-                swap_nodes(list, &curr, &curr->next);
-                print_list(*list);
-                swapped = 1;
-            }
-        }
-        end = curr;
-
-        if (swapped == 0)
-            break;
-
-        swapped = 0;
-        for (curr = end = end->prev; curr->prev != start->prev; curr = curr->prev)
-        {
-            if (curr->n < curr->prev->n)
-            {
-                swap_nodes(list, &curr->prev, &curr);
-                print_list(*list);
-                swapped = 1;
-            }
-        }
-        start = curr;
-    } while (swapped);
+	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
+		return;
+	node = *list;
+	while (swap_done == 1)
+	{
+		swap_done = 0;
+		while (node->next)
+		{
+			if (node->n > node->next->n)
+			{
+				node = swap_node(node->next, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			node = node->next;
+		}
+		if (swap_done == 0)
+			break;
+		swap_done = 0;
+		while (node->prev)
+		{
+			if (node->n < node->prev->n)
+			{
+				node = swap_node(node, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			else
+				node = node->prev;
+		}
+	}
 }
-
-/**
- * swap_nodes - swaps two nodes in a doubly linked list
- * @list: pointer to head of the doubly linked list
- * @node1: pointer to first node to swap
- * @node2: pointer to second node to swap
- */
-void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2)
-{
-    listint_t *temp = NULL;
-
-    if (*node1 == *node2)
-        return;
-
-    if ((*node1)->prev == NULL)
-    {
-        *list = *node2;
-        (*node2)->prev = NULL;
-    }
-    else if ((*node2)->prev == NULL)
-    {
-        *list = *node1;
-        (*node1)->prev = NULL;
-    }
-
-    if ((*node1)->next == *node2)
-    {
-        (*node1)->next = (*node2)->next;
-        (*node2)->prev = (*node1)->prev;
-        (*node1)->prev = *node2;
-        (*node2)->next = *node1;
-        if ((*node1)->next != NULL)
-            (*node1)->next->prev = *node1;
-        if ((*node2)->prev != NULL)
-            (*node2)->prev->next = *node2;
-    }
-    else
-    {
-        temp = (*node1)->next;
-        (*node1)->next = (*node2)->next;
-        (*node2)->next = temp;
-        temp = (*node1)->prev;
-        (*node1)->prev = (*node2)->prev;
-        (*node2)->prev = temp;
-        if ((*node1)->next != NULL)
-            (*node1)->next->prev = *node1;
-        if ((*node2)->next != NULL)
-            (*node2)->next->prev = *node2;
-        if ((*node1)->prev != NULL)
-            (*node1)->prev->next = *node1;
-        if ((*node2)->prev != NULL)
-            (*node2)->prev->next = *node2;
-    }
-}
-
