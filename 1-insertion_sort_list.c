51831cbad0ac967c65e8b67d1993cc55d7e5232f
WanjiruMuia <117749190+WanjiruMuia@users.noreply.github.com>
2023-04-24 23:44:46 +0300

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,41 +3,36 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       in ascending order using the Insertion sort algorithm
- * @list: Pointer to pointer to the head of the doubly linked list
- */
+* insertion_sort_list - Sorts a doubly linked list of integers
+*                       in ascending order using the Insertion sort algorithm
+* @list: Pointer to pointer to the head of the doubly linked list
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
-
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-
-    current = (*list)->next;
-
-    while (current != NULL)
-    {
-        temp = current->prev;
-        while (temp != NULL && temp->n > current->n)
-        {
-            /* Swap the nodes */
-            if (temp->prev != NULL)
-                temp->prev->next = current;
-            else
-                *list = current;
-
-            current->prev = temp->prev;
-            temp->prev = current;
-            temp->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = temp;
-            current->next = temp;
-
-            /* Print the list after each swap */
-            print_list(*list);
-            temp = current->prev;
-        }
-        current = current->next;
-    }
+listint_t *current, *temp;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+return;
+current = (*list)->next;
+while (current != NULL)
+{
+temp = current->prev;
+while (temp != NULL && temp->n > current->n)
+{
+/* Swap the nodes */
+if (temp->prev != NULL)
+temp->prev->next = current;
+else
+*list = current;
+current->prev = temp->prev;
+temp->prev = current;
+temp->next = current->next;
+if (current->next != NULL)
+current->next->prev = temp;
+current->next = temp;
+/* Print the list after each swap */
+print_list(*list);
+temp = current->prev;
+}
+current = current->next;
+}
 }
